a2d3d7cb6b9e78c4e4dd9ca44875f9c51d996766
polytheshepard <katies.truong@gmail.com>
2017-08-27 17:02:11 +1000

    Update BookingSystem.java

--- BookingSystem.java
+++ BookingSystem.java
@@ -1,16 +1,6 @@
 /*
  * class BookingSystem
  * 
- * This code is a partially complete application for the booking system which 
- * implements a fully functional menu for CPT121 Assignment 3 in SP2 2017.
- * 
- * You are expected to work off this sample implementation of the BookingSystem
- * class - you *DO NOT* need to implement your own version of this program menu.
- * 
- * Note that the required program features as described in Stages 2 and 4 of the
- * Assignment 3 specification should be implemented in the corresponding helper 
- * methods further * down in the class.
- * 
  */
 
 import java.util.Scanner;
@@ -18,19 +8,9 @@ import java.io.*;
 
 public class BookingSystem
 {
-    // Declare the array of Tour references in which the Tour and
-    // ScheduledTour objects that the user adds will be stored in and
-    // the corresponding tour count.
-    //
-    // Note that this array and the corresponding counter will be
-    // accessible both from within the main method as well as from
-    // within any helper methods you may decide to implement.
+   // Defines attraction array.
     private static final Attraction[] attractionList = new Attraction[100];
     private static int attractionCount = 0;
-
-    // Declaring a shared scanner just in case you choose to
-    // implement some helper methods in your application class
-    // that need access to one.
 
     private static final Scanner sc = new Scanner(System.in);
 
@@ -54,14 +34,14 @@ public class BookingSystem
             System.out.println("X - Exit Program");
             System.out.println();
 
-            // prompt the user to enter their selection
+            // Prompts the user to enter their selection.
             System.out.print("Enter your selection: ");
             input = sc.nextLine();
 
             System.out.println();
 
-            // check to see if the user failed to enter exactly one character
-            // for their menu selection
+            // Check to see if the user failed to enter exactly one character
+            // for their menu selection.
 
             if (input.length() != 1)
             {
@@ -69,10 +49,9 @@ public class BookingSystem
             }
             else
             {
-                // extract the user's menu selection as a char value and
+                // Extract the user's menu selection as a char value and
                 // convert it to upper case so that the menu becomes
-                // case-insensitive
-
+                // case-insensitive.
                 selection = Character.toUpperCase(input.charAt(0));
 
                 // process the user's selection
@@ -80,8 +59,8 @@ public class BookingSystem
                 {
                     case 'A':
                     case 'a':
-                    // call addNewAttraction() helper method
-                    addNewAttraction();
+                        // call addNewAttraction() helper method
+                        addNewAttraction();
                     break;
                     case 'B':
                     case 'b':
@@ -91,7 +70,7 @@ public class BookingSystem
                     case 'C':
                     case 'c':
                         // call sellPasses() helper method
-                            sellPasses();
+                        sellPasses();
                         break;
                     case 'D':
                     case 'd':
@@ -130,9 +109,6 @@ public class BookingSystem
     */
    private static final void addNewAttraction()
    {
-       // Implement your code for Stage 2 Requirement A) here
-       // prompts user to add their ID, description and admission fee
-       // when adding new Attraction
        String id, description;
        double admissionFee;
        Attraction a;
@@ -184,8 +160,6 @@ public class BookingSystem
     */
    private static final void displayAttractionSummary()
    {
-      // Implement your code for Stage 2 Requirement B) here
-      //
       System.out.println("Display Attraction Summary Feature Selected!");
       for (int i = 0; i < attractionCount; i++) {
           attractionList[i].printDetails();
@@ -206,9 +180,6 @@ public class BookingSystem
 
    private static final void sellPasses() throws BookingException
    {
-
-      // Implement your code for Stage 2 Requirement C) here
-      // ...
       System.out.println("Sell Passes Feature Selected!");
       String id;
       int numTourists;
@@ -255,8 +226,6 @@ public class BookingSystem
     */
    private static final void refundPasses()
    {
-      // Implement your code for Stage 2 Requirement D) here
-      // ...
       System.out.println("Refund Passes Feature Selected!");
       String id;
       int numTourists;
@@ -300,8 +269,6 @@ public class BookingSystem
 
    private static void addNewScheduledTour()
    {
-      // Implement your code for Stage 4 Requirement A) here
-      // Prompts user to input details for tour features
       System.out.println("Add New Scheduled Tour Feature Selected!");
       String tourID, tourDescription, tourDate, tourGuide;
       double tourAdmissionFee;
@@ -363,8 +330,6 @@ public class BookingSystem
     */
    private static void updateMaxGroupSize()
    {
-      // Implement your code for Stage 4 Requirement B) here
-      // ...
       System.out.println("Update Maximum Group Size Feature Selected!");
       Attraction a;
       ScheduledTour s = null;
